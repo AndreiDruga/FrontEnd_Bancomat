8249c3ef9695804c5b65514a322038ccaf0ef78d
AndreiDruga <75364827+AndreiDruga@users.noreply.github.com>
2021-11-04 20:07:14 +0200

    hhhhhhhhh

--- pagini/main2.js
+++ pagini/main2.js
@@ -1,73 +1,112 @@
-// Create a registration system
-var objPeople = [
-	{
-		exampleInputCardNumber: '1234',
-		exempleInputPin: '12345678'
-	},
-	{
-		exampleInputCardNumber: 'matt',
-		exempleInputPin: 'password88'
-	},
-	{
-		exampleInputCardNumber: 'chris',
-		exempleInputPin: 'password3'
-	}
-]
-// login functionality
-function login() {
-	// retreive data from username and store in username variable
-	var exampleInputCardNumber = document.getElementById('exampleInputCardNumber').value
-	// retreive data from password and store in password variable
-	var exempleInputPin = document.getElementById('exempleInputPin').value
 
-	// loop through all the user pbjects and confrim if the username and password are correct
-	for(var i = 0; i < objPeople.length; i++) {
-		// check to 
-		if(exampleInputCardNumber == objPeople[i].exampleInputCardNumber && exempleInputPin== objPeople[i].exempleInputPin) {
-			console.log(exampleInputCardNumber + ' is logged in!!!')
-			window.location = "D:\\repo\\FrontEnd_Bancomat\\pagini\\menuPage.html";
-			alert("Login was successful");
-			// stop the statement if result is found true - this was a return in the video, break is best practice here
-			break
-		} else {
-			// error if username and password don't match
-			console.log('incorrect card number or pin')
-			alert("incorrect card number or pin was successful");
-		}
-    }
-} 
-// register functionality
+var objPeople = [
+  {
+    cardNumber: "1234",
+    pin: "12345678",
+    sold:"123",
+  },
+  {
+    cardNumber: "123456",
+    pin: "123456789",
+    sold:"1230",
+  },
+  {
+    cardNumber: "chris",
+    pin: "password3",
+    sold:"123",
+  },
+];
+
+function init() {
+  
+  
+  if (
+    window.localStorage.getItem("cards") === undefined ||
+    window.localStorage.getItem("cards") === null
+  ) {
+    window.localStorage.setItem("cards", JSON.stringify(objPeople));
+  }
+}
+
+// log in card user
+function login() {
+  var cardNumber = document.getElementById("exampleInputCardNumber").value;
+  var pin = document.getElementById("exempleInputPin").value;
+  const existingCards = getCardsFromMemory();
+  const existingCard = existingCards.find(
+    (element) => element.cardNumber === cardNumber && element.pin === pin 
+    );
+  const index =  existingCards.findIndex(
+  (element) => element.cardNumber === cardNumber && element.pin === pin 
+  );
+  var sold=objPeople[index].sold;
+  if (existingCard === undefined) {
+    alert("incorrect card number or pin");
+  } else {
+    curentCard(cardNumber,pin,sold);
+    window.location = "./menuPage.html";
+     alert("Login was successful");
+  }
+}
+
+// register user
 function registerUser() {
-    // store new users username
-	var registerCardNumber = document.getElementById('newCardNumber').value
-	// store new users password
-	var registerPin = document.getElementById('newPin').value
-	// store new user data in an object
-	var newCardNumber = {
-	 exampleInputCardNumber: registerCardNumber,
-	 exempleInputPin: registerPin
-    }
-    // loop throught people array to see if the username is taken, or password to short
-    for(var i = 0; i < objPeople.length; i++) {
-	 // check if new username is equal to any already created usernames
-     if(registerUser == objPeople[i].exampleInputCardNumber) {
-	      // alert user that the username is take
-	      alert('That card number is alreat in user, please choose another')
-	       // stop the statement if result is found true
-	     break
-         // check if new password is 8 characters or more
-	    } 
-	 else if (registerPin.length < 8) {
-	        // alert user that the password is to short
-	     alert('That is to short, include 8 or more characters')
-	     // stop the statement if result is found true
-	     break
-	    }
-	}
-	alert('Register sucesfful')
-	window.location = "D:\\repo\\FrontEnd_Bancomat\\pagini\\menuPage.html";
-	// push new object to the people array
-	objPeople.push(newCardNumber)
-	// console the updated people array
-	console.log(objPeople)	
-	}
+  var newCardNumber = {
+    cardNumber: document.getElementById("newCardNumber").value,
+    pin: document.getElementById("newPin").value,
+  };
+  const existingCards = getCardsFromMemory();
+  const existingCard = existingCards.find(
+    (element) => element.cardNumber === newCardNumber.cardNumber
+  );
+
+  if (existingCard !== undefined) {
+	alert("That card number is alreat in user, please choose another");
+  } else if (newPin.length < 8) {
+	alert("That is to short, include 8 or more characters");
+  }
+  alert("Register sucesfful");
+  window.location = "./menuPage.html";
+  existingCards.push(newCardNumber);
+  updateCardsInMemory(existingCards);
+}
+
+function curentCard(cardNumber,pin,sold){
+  var card={
+    cardNumber:cardNumber,
+    pin:pin,
+    sold:sold,
+  }
+  window.localStorage.setItem("curentCard", JSON.stringify(card));
+}
+
+function getCardsFromMemory() {
+  return JSON.parse(window.localStorage.getItem("cards"));
+}
+
+function updateCardsInMemory(updatedCardsArray) {
+  window.localStorage.setItem("cards", JSON.stringify(updatedCardsArray));
+}
+
+function exit(){
+  localStorage.clear();
+}
+
+function changePin(){
+  const curentCard=JSON.parse(window.localStorage.getItem("curentCard"));
+  console.log(curentCard.pin)
+}
+
+function seeSold(){
+  const curentCard=JSON.parse(window.localStorage.getItem("curentCard"));
+  document.getElementById("sold").innerHTML=curentCard.sold;
+}
+
+function incrementSold(){
+ const curentCard=JSON.parse(window.localStorage.getItem("curentCard"));
+ curentCard.sold=parseInt(curentCard.sold)    +
+ parseInt(document.getElementById("newSold").value);
+  console.log(curentCard.sold);
+ updateCardsInMemory(curentCard);
+}
+
